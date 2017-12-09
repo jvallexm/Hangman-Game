1534e03e1bae24f3d571973e2cb17c1c32a96564
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-09 09:55:27 -0500

    arranged functions into object

--- assets/script.js
+++ assets/script.js
@@ -3,122 +3,108 @@ var word = "almond";
 
 // Display of the unsolved word
 var wordDisplay = "";
+
 // Incorrect guesses
 var wrongs = 0;
+
 // Guessed letters
 var guessed = [];
+
 // Has the game started ? 
 var start = false;
+
 // Total player wins
 var wins = 0;
+
 // Player fails
 var fails = 0;
 
 // Updates the display based on guessed letters
-var updateDisplay = () =>{
+
+const hangman = {
+
+    // Checks both single letters and the string entered in the input box
+
+    checkLetter: (char) => {
+
+        let lower = char.toLowerCase();
+        let check = 0;
+        $("#" + lower).css("color","red");
+        if(guessed.indexOf(lower) == -1)
+          guessed.push(lower);
+        if(word.indexOf(lower) != -1)
+          wrongs++; 
+        hangman.updateDisplay();
+    },
+
+    //
+
+    keyup: (e) =>{
+        if(!start){
+          start = true;
+          hangman.updateDisplay();
+        } else if(/[a-zA-Z]/.test(e.key) && e.key.length === 1) {
+          console.log(e.key + " is a letter");
+          if(guessed.indexOf(e.key) == -1)
+            hangman.checkLetter(e.key.toLowerCase());
+        }
+    },
+
+    // Select a new word after the game has ended
+
+    newGame: () => {
+      console.log("this is where the game would start");
+      for(let i=0;i<guessed.length;i++){
+        console.log(i);
+        $("#" + guessed[i]).css("color","black");
+      }
+      guessed = [];
+      wrongs = 0;
+      word = "parakeet";
+      hangman.updateDisplay();
+    },
+
+    updateDisplay: () =>{
   
-  let splitDisplay = word.split("");
+        let splitDisplay = word.split("");
 
-  // Replaces letters not in guessed with "_"
+        // Replaces letters not in guessed with "_"
 
-  for(let i=0;i<splitDisplay.length;i++) {
-    if(guessed.indexOf(splitDisplay[i]) == -1 && splitDisplay[i] != " ")
-      splitDisplay[i] = "_";
-  }
+        for(let i=0;i<splitDisplay.length;i++) {
+          if(guessed.indexOf(splitDisplay[i]) == -1 && splitDisplay[i] != " ")
+            splitDisplay[i] = "_";
+        }
 
-  wordDisplay = splitDisplay.join("");
+        wordDisplay = splitDisplay.join("");
 
-  // Updates the word display
+        // Updates the word display
 
-  document.getElementById("display").textContent = wordDisplay;
-  document.getElementById("wrongs").textContent = wrongs;
-  document.getElementById("wins").textContent = wins;
-  document.getElementById("fails").textContent = fails;
+        document.getElementById("display").textContent = wordDisplay;
+        document.getElementById("wrongs").textContent = wrongs;
+        document.getElementById("wins").textContent = wins;
+        document.getElementById("fails").textContent = fails;
 
-  // Checks to see if the puzzle is complete
-  if(wordDisplay.indexOf("_")==-1 || wrongs > 10)
-  {
-    if(wrongs > 10)
-    {
-      fails++;
+        // Checks to see if the puzzle is complete
+        if(wordDisplay.indexOf("_")==-1 || wrongs > 10)
+        {
+          if(wrongs > 10)
+          {
+            fails++;
+          }
+          else
+          {
+            wins++;
+          }
+          hangman.newGame();
+        }
     }
-    else
-    {
-      wins++;
-    }
-    newGame();
-  }
+
 }
 
-var newGame = () =>{
-  console.log("this is where the game would start");
-  for(let i=0;i<guessed.length;i++){
-    console.log(i);
-    $("#" + guessed[i]).css("color","black");
-  }
-  guessed = [];
-  wrongs = 0;
-  word = "parakeet";
-  updateDisplay();
-}
-
-// Runs the first display update on load
-// updateDisplay();
-
- // Checks both single letters and the string entered in the input box
-var checkSplitLetters = (str) => {
-  
-  if(!start) {
-    
-    start = true;
-    
-  } else {
-    
-    let split = str.toLowerCase().split("");
-    let check = 0;
-    for(let i=0;i<split.length;i++) {
-      
-      $("#" + split[i]).css("color","red");
-      if(guessed.indexOf(split[i]) == -1)
-        guessed.push(split[i]);
-      if(word.indexOf(split[i]) != -1)
-        check++;
-    }
-    if(check == 0)
-      wrongs++;
-    
-  }
-  updateDisplay();
-}
-
-var convertInput = () =>{
-  
-  // Converts the input box into a string 
-  let input = document.getElementById("input").value;
-  if(input=="")
-    return;
-  console.log("input: " + input);
-  checkSplitLetters(input);
-  document.getElementById("input").value = "";
-  
-}
-
-// Handles clicking the submit button
-document.getElementById("submit").onclick = ()=>{
-  convertInput(); 
-}
-
-document.onkeyup = (e) =>{
-    if(/[a-zA-Z]/.test(e.key) && e.key.length === 1)
-    {
-      console.log(e.key + " is a letter");
-      if(guessed.indexOf(e.key) == -1 )
-        checkSplitLetters(e.key.toLowerCase());
-    }
-}
+document.onkeyup = hangman.keyup;
 
 // Checks letters when the letter buttons are clicked on
 $(".letter").click((e)=>{
   if(guessed.indexOf(e.target.id) == -1)
-  checkSplitLetters(e.target.id);
+  hangman.checkLetter(e.target.id);
 });
